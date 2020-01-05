de6ec0d508aa183477128361537c3e8ede9c697d
David Vega <dvega920@gmail.com>
2020-01-05 10:28:50 -0500

    font-end dom manip using jquery click events ajax calls

---
+++ public/assets/js/burger.js
@@ -0,0 +1,64 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".eat-burger").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        // var newSleep = $(this).data("newsleep");
+
+        var devouredState = {
+            devoured: 1
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredState
+        }).then(
+            function () {
+                console.log("Burger eaten!");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#newburger").val().trim(),
+            devoured: $("[name:devoured]:checked").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("Added new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("Deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
