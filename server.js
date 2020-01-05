84d5530207e7c16f00767d084435c9838862b47e
David Vega <dvega920@gmail.com>
2020-01-05 05:36:14 -0500

    setup required dep, port, middleware, routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+//Sets up express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+})
