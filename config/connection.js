3715bec02e4e365fcb70aaf5c577b9da5f90b4da
David Vega <dvega920@gmail.com>
2020-01-05 23:24:34 -0500

    added JAWSDB enviro var

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Dv3ga75579dv6380!",
-    database: "burgers_db"
-});
+const connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Dv3ga75579dv6380!",
+        database: "burgers_db"
+    });
+}
 connection.connect()
 
 module.exports = connection;
